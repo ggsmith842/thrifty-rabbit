dfb784810123c30db86b33be33d08bead07f0a4f
ggsmith842 <ggsmith@ucdavis.edu>
2023-07-28 17:50:46 -0600

    update case on goals variables

--- src/goals.py
+++ src/goals.py
@@ -16,57 +16,57 @@ class Goal:
     def __init__(
         self,
         name,
-        targetYear,
-        targetValue,
-        initialContribution=0,
-        monthlyContribution=0,
+        target_year,
+        target_value,
+        initial_contribution=0,
+        monthly_contribution=0,
         priority="",
     ):
         self.name = name
-        self.targetYear = targetYear
-        self.targetValue = targetValue
-        self.initialContribution = initialContribution
-        self.monthlyContribution = monthlyContribution
-        if not (priority == "") and not (
-            priority in ["Dreams", "Wishes", "Wants", "Needs"]
+        self.target_year = target_year
+        self.target_value = target_value
+        self.initial_contribution = initial_contribution
+        self.monthly_contribution = monthly_contribution
+        if  (priority != "") and (
+            priority not in ["Dreams", "Wishes", "Wants", "Needs"]
         ):
             raise ValueError("Wrong value set for Priority.")
         self.priority = priority
 
-    def getGoalProbabilities(self):
+    def get_goal_probabilities(self):
         '''
         Calculates the probability of reaching a goal.
         '''
         if self.priority == "":
             raise ValueError("No value set for Priority.")
-        lookupTable = pd.read_csv("../Data/goalprobabilities.csv")
-        match = lookupTable["Realize"] == self.priority
-        minProb = lookupTable["MinP"][(match)]
-        maxProb = lookupTable["MaxP"][(match)]
-        return minProb.values[0], maxProb.values[0]
+        lookup_table = pd.read_csv("../Data/goalprobabilities.csv")
+        match = lookup_table["Realize"] == self.priority
+        min_prob = lookup_table["MinP"][(match)]
+        max_prob = lookup_table["MaxP"][(match)]
+        return min_prob.values[0], max_prob.values[0]
 
 
 class RetirementGoal(Goal):
     '''
     Holds information for specific goal of type retirement.
     '''
-    def __init__(self, name, targetValue, startingAge, retirementAge):
-        targetYear = date.today().year + (retirementAge - startingAge)
-        super().__init__(name, targetYear, targetValue)
-        self.retirementAge = retirementAge
+    def __init__(self, name, target_value, startingAge, retirementAge):
+        target_year = date.today().year + (retirementAge - startingAge)
+        super().__init__(name, target_year, target_value)
+        self.retirement_age = retirementAge
 
 
 class GrowWelathGoal(Goal):
     '''
     Holds information for specific goal of type grow wealth.
     '''
-    def __init__(self, initialContribution, monthlyContribution):
-        targetYear = date.today().year + 10
-        targetAmount = 1000000
+    def __init__(self, initial_contribution, monthly_contribution):
+        target_year = date.today().year + 10
+        target_amount = 1000000
         super().__init__(
             "Grow My Wealth",
-            targetYear,
-            targetAmount,
-            initialContribution,
-            monthlyContribution,
+            target_year,
+            target_amount,
+            initial_contribution,
+            monthly_contribution,
         )
